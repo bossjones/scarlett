4e4223f6db1b61ea707760d7d9890948ba4399ed
Malcolm Jones <malcolm@adobe.com>
2014-10-13 16:15:17 -0400

    style: copyright stuff @wip

--- scarlett/features/forecast.py
+++ scarlett/features/forecast.py
@@ -11,20 +11,6 @@ class FeatureForecast(Feature):
     capability = []
 
     def __init__(self, name):
-        """Constructs the handlers.
-        :type host: string
-        :param host: The host to which the request is being sent.
-
-        :type config: boto.pyami.Config
-        :param config: Boto configuration.
-
-        :type provider: boto.provider.Provider
-        :param provider: Provider details.
-
-        Raises:
-            NotReadyToAuthenticate: if this handler is not willing to
-                authenticate for the given provider and config.
-        """
 
         ## REFACTOR NEEDED # self.lat                = self.config.get('forecastio','lat')
         ## REFACTOR NEEDED # self.lng                = self.config.get('forecastio','lng')
@@ -35,11 +21,6 @@ class FeatureForecast(Feature):
         Feature.__init__(self, "forecast")
 
     def add_auth(self, http_request):
-        """Invoked to add authentication details to request.
-
-        :type http_request: boto.connection.HTTPRequest
-        :param http_request: HTTP request that needs to be authenticated.
-        """
         pass
 
     def forecast_play(self, cmd):
--- scarlett/features/time.py
+++ scarlett/features/time.py
@@ -5,28 +5,9 @@ class FeatureTime(Feature):
     capability = []
 
     def __init__(self, host, config, provider):
-        """Constructs the handlers.
-        :type host: string
-        :param host: The host to which the request is being sent.
-
-        :type config: boto.pyami.Config
-        :param config: Boto configuration.
-
-        :type provider: boto.provider.Provider
-        :param provider: Provider details.
-
-        Raises:
-            NotReadyToAuthenticate: if this handler is not willing to
-                authenticate for the given provider and config.
-        """
         Feature.__init__(self, "time")
 
     def add_auth(self, http_request):
-        """Invoked to add authentication details to request.
-
-        :type http_request: boto.connection.HTTPRequest
-        :param http_request: HTTP request that needs to be authenticated.
-        """
         pass
 
     def time_play(self,cmd):
