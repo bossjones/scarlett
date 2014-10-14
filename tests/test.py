6056f98f5511d104bfd9730ccaf9ffb45fb9c368
Malcolm Jones <malcolm@adobe.com>
2014-10-14 15:11:21 -0400

    cng: added pocketsphinx to travis.yml and switched around how test.py wrapper works @chg @fix @wip

--- tests/test.py
+++ tests/test.py
@@ -43,16 +43,5 @@ def main():
     else:
         return 1
 
-class TestScarlett(unittest.TestCase):
-
-    def setUp(self):
-        pass
-
-    def test_something(self):
-        pass
-
-    def tearDown(self):
-        pass
-
 if __name__ == '__main__':
     unittest.main()
---
+++ tests/unit/__init__.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# This file is part of Scarlett.
+# Copyright 2014, Behanceops.
+
+"""Some common functionality for Scarlett test cases."""
+
+# Use unittest2 on Python < 2.7.
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+import scarlett
+import os
+
+class ScarlettTestCase(unittest.TestCase):
+
+    """A unittest.TestCase subclass that saves and restores Anaconda
+    global configuration. This allows tests to make temporary
+    modifications that will then be automatically removed when the test
+    completes. Hardcoding with mir for the moment till everything works
+    """
+
+    def setUp(self):
+        # this will skip the scarlett_test_init function
+        os.environ['SKIP_SCARLETT_INIT'] = '1'
+
+    """
+    For the moment this isn't the best tearDown function,
+    but we should keep it to remind ourselves to add more to it
+    in the future
+    """
+
+    def tearDown(self):
+        del os.environ['SKIP_SCARLETT_INIT']
+
+    def assertExists(self, path):
+        self.assertTrue(os.path.exists(path),
+                        'file does not exist: %s' % path)
+
+    def assertNotExists(self, path):
+        self.assertFalse(os.path.exists(path),
+                         'file exists: %s' % path)
