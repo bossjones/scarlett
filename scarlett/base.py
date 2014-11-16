25088ae8a07c036bec226199cfd0280a7726350a
Malcolm Jones <malcolm@behance.com>
2014-11-16 18:06:50 -0500

    wip: commented out gevent for moment, dont enabled without handling zombie threads

--- scarlett/base.py
+++ scarlett/base.py
@@ -1,33 +1,34 @@
-import scarlett
-from scarlett.constants import *
-import threading
-import os
-import time
-import gevent
-gevent.monkey.patch_all()
-from gevent.socket import wait_read
-import redis
-import redis.connection
-redis.connection.socket = gevent.socket
-
-class ScarlettBase(object):
-    """
-    Class to route Event communication to all parts of scarlett
-    """
-
-    def __init__(self):
-      scarlett.set_stream_logger('scarlett')
-      scarlett.log.debug(Fore.YELLOW + "Inside ScarlettBase")
-      self.config       = scarlett.config
-      self.redis_host   = self.scarlett.config.get('redis','host')
-      self.redis_port   = self.scarlett.config.get('redis','port')
-      self.redis_db     = self.scarlett.config.get('redis','db')
-      #self.redis_server = getdb("redis://%s:%i?timeout=%i" % (self.redis_host,self.redis_port,self.redis_db) )
-      self.redis_server = redis.Redis(host='localhost',port=6379,db=0)
-      self.pubsub       = self.redis_server.pubsub()
-
-    def get_redis_server(self):
-        return self.redis_server
-
-    def my_handler(message):
-        scarlett.log.debug(Fore.YELLOW + "MY HANDLER" + message['data'])
+# disabled ########## import scarlett
+# disabled ########## from scarlett.constants import *
+# disabled ########## import threading
+# disabled ########## import os
+# disabled ########## import time
+# disabled ########## import gevent
+# disabled ########## gevent.monkey.patch_all()
+# disabled ########## from gevent.socket import wait_read
+# disabled ########## import redis
+# disabled ########## import redis.connection
+# disabled ########## redis.connection.socket = gevent.socket
+# disabled ##########
+# disabled ########## class ScarlettBase(object):
+# disabled ##########     """
+# disabled ##########     Class to route Event communication to all parts of scarlett
+# disabled ##########     """
+# disabled ##########
+# disabled ##########     def __init__(self):
+# disabled ##########       scarlett.set_stream_logger('scarlett')
+# disabled ##########       scarlett.log.debug(Fore.YELLOW + "Inside ScarlettBase")
+# disabled ##########       self.config       = scarlett.config
+# disabled ##########       self.redis_host   = self.scarlett.config.get('redis','host')
+# disabled ##########       self.redis_port   = self.scarlett.config.get('redis','port')
+# disabled ##########       self.redis_db     = self.scarlett.config.get('redis','db')
+# disabled ##########       #self.redis_server = getdb("redis://%s:%i?timeout=%i" % (self.redis_host,self.redis_port,self.redis_db) )
+# disabled ##########       self.redis_server = redis.Redis(host='localhost',port=6379,db=0)
+# disabled ##########       self.pubsub       = self.redis_server.pubsub()
+# disabled ##########
+# disabled ##########     def get_redis_server(self):
+# disabled ##########         return self.redis_server
+# disabled ##########
+# disabled ##########     def my_handler(message):
+# disabled ##########         scarlett.log.debug(Fore.YELLOW + "MY HANDLER" + message['data'])
+# disabled ##########
--- scarlett/brain/__init__.py
+++ scarlett/brain/__init__.py
@@ -1,101 +1,101 @@
-#!/usr/bin/env python
-
-"""
-Scarlett Brain
-"""
-
-from __future__ import unicode_literals
-import threading
-import os
-import time
-import gevent
-from gevent import monkey
-gevent.monkey.patch_all()
-import redis
-import redis.connection
-redis.connection.socket = gevent.socket
-import scarlett
-from scarlett.constants import *
-import redis
-from json import loads, dumps
-
-class ScarlettBrain(object):
-
-    def __init__(self):
-      self.config       = scarlett.config
-      self.redis_host   = self.scarlett.config.get('redis','host')
-      self.redis_port   = self.scarlett.config.get('redis','port')
-      self.redis_db     = self.scarlett.config.get('redis','db')
-      self.redis_server = None
-      self.pubsub = None
-      scarlett.log.debug(Fore.YELLOW + "initializing ScarlettBrain")
-      #self.redis_server = redis.Redis(host=self.redis_host, port=self.redis_port , db=self.redis_db)
-      #self.redis_server.set("name", "ScarlettBrain")
-
-    def create_brain(self):
-        self.redis_server = redis.Redis(host=self.redis_host, port=self.redis_port , db=self.redis_db)
-        self.redis_server.set("name", "ScarlettBrain")
-        scarlett.log.debug(Fore.YELLOW + "create_brain")
-        return self.redis_server
-
-    def get_brain(self):
-      return self.redis_server
-
-    #def create_brain_event_listener(self):
-    #    if hasattr(self,"self.redis_server"):
-    #       self.pubsub = self.get_brain().pubsub()
-    #    else:
-    #       self.pubsub = self.create_brain().pubsub()
-#
-    #    self.pubsub.subscribe('scarlett-brain')
-    #    thread = p.run_in_thread(sleep_time=0.001)
-    #    self.redis_server.publish('scarlett-brain')
-#
-    #    return self.pubsub
-
-    def get_brain_event_listener(self):
-        return self.pubsub
-
-    def sub_brain_event_listener(self):
-        return self.create_brain().pubsub()
-
-    def set_keyword_identified(self,keyword_value):
-        return self.redis_server.set("keyword_identified", keyword_value)
-
-    def get_keyword_identified(self):
-        return self.redis_server.get("keyword_identified")
-
-    def set_brain_item(self,key,value):
-        return self.redis_server.set(key, value)
-
-    def get_brain_item(self,key):
-        return self.redis_server.get(key)
-
-    def remove_brain_item(self,key):
-        return self.redis_server.delete(key)
-
-    def set_service_identified(self,service_name,key):
-        return self.redis_server.set("service_%s"%(service_name),service_identified)
-
-    def incr_service_identified(self,service_name):
-        return self.redis_server.incr("service_%s"%(service_name))
-
-    def decr_service_identified(self,service_name):
-        return self.redis_server.decr("service_%s"%(service_name))
-
-    def get_service_identified(self,service_name):
-        return self.redis_server.get("service_%s"%(service_name),service_identified)
-
-    def wipe_brain(self):
-      self.redis_server.flushall()
-
-    def start_subscriber():
-        crongreenlet = gevent.spawn(cron)
-
-    def cron():
-        while True:
-            scarlett.log.debug(Fore.YELLOW + "Hello from cron greenlet.")
-            gevent.sleep(0.2)
+# DISABLED #### #!/usr/bin/env python
+# DISABLED ####
+# DISABLED #### """
+# DISABLED #### Scarlett Brain
+# DISABLED #### """
+# DISABLED ####
+# DISABLED #### from __future__ import unicode_literals
+# DISABLED #### import threading
+# DISABLED #### import os
+# DISABLED #### import time
+# DISABLED #### import gevent
+# DISABLED #### from gevent import monkey
+# DISABLED #### gevent.monkey.patch_all()
+# DISABLED #### import redis
+# DISABLED #### import redis.connection
+# DISABLED #### redis.connection.socket = gevent.socket
+# DISABLED #### import scarlett
+# DISABLED #### from scarlett.constants import *
+# DISABLED #### import redis
+# DISABLED #### from json import loads, dumps
+# DISABLED ####
+# DISABLED #### class ScarlettBrain(object):
+# DISABLED ####
+# DISABLED ####     def __init__(self):
+# DISABLED ####       self.config       = scarlett.config
+# DISABLED ####       self.redis_host   = self.scarlett.config.get('redis','host')
+# DISABLED ####       self.redis_port   = self.scarlett.config.get('redis','port')
+# DISABLED ####       self.redis_db     = self.scarlett.config.get('redis','db')
+# DISABLED ####       self.redis_server = None
+# DISABLED ####       self.pubsub = None
+# DISABLED ####       scarlett.log.debug(Fore.YELLOW + "initializing ScarlettBrain")
+# DISABLED ####       #self.redis_server = redis.Redis(host=self.redis_host, port=self.redis_port , db=self.redis_db)
+# DISABLED ####       #self.redis_server.set("name", "ScarlettBrain")
+# DISABLED ####
+# DISABLED ####     def create_brain(self):
+# DISABLED ####         self.redis_server = redis.Redis(host=self.redis_host, port=self.redis_port , db=self.redis_db)
+# DISABLED ####         self.redis_server.set("name", "ScarlettBrain")
+# DISABLED ####         scarlett.log.debug(Fore.YELLOW + "create_brain")
+# DISABLED ####         return self.redis_server
+# DISABLED ####
+# DISABLED ####     def get_brain(self):
+# DISABLED ####       return self.redis_server
+# DISABLED ####
+# DISABLED ####     #def create_brain_event_listener(self):
+# DISABLED ####     #    if hasattr(self,"self.redis_server"):
+# DISABLED ####     #       self.pubsub = self.get_brain().pubsub()
+# DISABLED ####     #    else:
+# DISABLED ####     #       self.pubsub = self.create_brain().pubsub()
+# DISABLED #### #
+# DISABLED ####     #    self.pubsub.subscribe('scarlett-brain')
+# DISABLED ####     #    thread = p.run_in_thread(sleep_time=0.001)
+# DISABLED ####     #    self.redis_server.publish('scarlett-brain')
+# DISABLED #### #
+# DISABLED ####     #    return self.pubsub
+# DISABLED ####
+# DISABLED ####     def get_brain_event_listener(self):
+# DISABLED ####         return self.pubsub
+# DISABLED ####
+# DISABLED ####     def sub_brain_event_listener(self):
+# DISABLED ####         return self.create_brain().pubsub()
+# DISABLED ####
+# DISABLED ####     def set_keyword_identified(self,keyword_value):
+# DISABLED ####         return self.redis_server.set("keyword_identified", keyword_value)
+# DISABLED ####
+# DISABLED ####     def get_keyword_identified(self):
+# DISABLED ####         return self.redis_server.get("keyword_identified")
+# DISABLED ####
+# DISABLED ####     def set_brain_item(self,key,value):
+# DISABLED ####         return self.redis_server.set(key, value)
+# DISABLED ####
+# DISABLED ####     def get_brain_item(self,key):
+# DISABLED ####         return self.redis_server.get(key)
+# DISABLED ####
+# DISABLED ####     def remove_brain_item(self,key):
+# DISABLED ####         return self.redis_server.delete(key)
+# DISABLED ####
+# DISABLED ####     def set_service_identified(self,service_name,key):
+# DISABLED ####         return self.redis_server.set("service_%s"%(service_name),service_identified)
+# DISABLED ####
+# DISABLED ####     def incr_service_identified(self,service_name):
+# DISABLED ####         return self.redis_server.incr("service_%s"%(service_name))
+# DISABLED ####
+# DISABLED ####     def decr_service_identified(self,service_name):
+# DISABLED ####         return self.redis_server.decr("service_%s"%(service_name))
+# DISABLED ####
+# DISABLED ####     def get_service_identified(self,service_name):
+# DISABLED ####         return self.redis_server.get("service_%s"%(service_name),service_identified)
+# DISABLED ####
+# DISABLED ####     def wipe_brain(self):
+# DISABLED ####       self.redis_server.flushall()
+# DISABLED ####
+# DISABLED ####     def start_subscriber():
+# DISABLED ####         crongreenlet = gevent.spawn(cron)
+# DISABLED ####
+# DISABLED ####     def cron():
+# DISABLED ####         while True:
+# DISABLED ####             scarlett.log.debug(Fore.YELLOW + "Hello from cron greenlet.")
+# DISABLED ####             gevent.sleep(0.2)
 
     # @property
     # def name(self):
