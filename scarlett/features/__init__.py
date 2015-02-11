2ad715b3780de9517447a7ce1ff82ef859c7f63b
Malcolm Jones <malcolm@adobe.com>
2015-02-11 18:55:41 -0500

    chg: UTF-8 added

--- scarlett/features/__init__.py
+++ scarlett/features/__init__.py
@@ -1,16 +1,4 @@
-# This file is part of scarlett.
-# Copyright 2014, Malcolm Jones.
-#
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-#
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
+# -*- coding: UTF-8 -*-
 
 import scarlett
 from scarlett.constants import *
